4182155d85b9d6e936bd951291041b06c1665c61
zfy <rachmanino@yahoo.com>
2020-04-25 12:27:27 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,6 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa_travis
 cp .travis/id_rsa_travis.pub ~/.ssh/
-# sudo chmod g-w /home
-# sudo chmod o-wx /home
 chmod g-w ~/
 chmod o-wx ~/
 chmod g-w ~/.ssh/
@@ -12,10 +10,13 @@ chmod o-wx ~/.ssh/config
 chmod g-w ~/.ssh/id_rsa_travis.pub
 chmod o-wx ~/.ssh/id_rsa_travis.pub
 
-cd ~/.gradle/
-ls -al
+sudo apt-get update -y
+sudo apt-get install -y pigz
 cd ~/
+tar -cf repo.tar.gz -I pigz .gradle
 
-# ssh -p 40501 qwe@198e3e504d5ee164.natapp.cc
-# scp -P 40501 -o stricthostkeychecking=no -r ~/.m2/repository/*.jar qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/jars/
-# rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/.m2/repository/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/jars2/
+dirs=(~/build/penelope24/*/)
+name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+
+rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" ~/repo.tar qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
+rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
